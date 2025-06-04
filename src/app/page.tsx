853df12bac43b6d2138d28759ceec9f29eb9f68e
akshay-rapidops <soniiakshayy8000@gmail.com>
2025-06-04 16:35:57 +0530

    #great commit after success
    #great commit after success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import React from 'react';
+import './globals.css'
 
 export default function Home() {
   return (
     <>
-      <h1>Learning AWS.</h1>
+        <div  className={'box'} >
+            <h1>Ohoo Great R!.</h1>
+
+        </div>
     </>
   );
 }
